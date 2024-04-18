8166bfacee31cd492c2b80169f247687db715d5b
Waji <76054852+Waji-97@users.noreply.github.com>
2024-04-18 10:37:38 +0900

    Update main.tf
    Test adding another worker for waji

--- modules/worker_node/main.tf
+++ modules/worker_node/main.tf
@@ -63,6 +63,71 @@ resource "proxmox_virtual_environment_vm" "wa-worker01" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "wa-worker02" {
+  name = "wa-worker02"
+  node_name = var.proxmox_server
+
+  cpu {
+    cores = 3
+  }
+
+  memory {
+    dedicated = 4096
+  }
+
+  initialization {
+    upgrade = false
+    ip_config {
+      ipv4 {
+        address = "192.168.219.45/24"
+        gateway = "192.168.219.1"
+      }
+    }
+
+    user_account {
+      username = "homelab"
+      keys = [trimspace(var.ssh_key)]
+    }
+  }
+
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id = var.ubuntu_image_file_id
+    interface = "virtio0"
+    iothread = true
+    size = 30
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+  provisioner "file" {
+    connection {
+    type = "ssh"
+    user = "homelab"
+    private_key = "${file("/home/homelab/.ssh/id_rsa")}"
+    host = "192.168.219.45"
+  }
+    source = "/home/homelab/k8s_init.sh"
+    destination = "/home/homelab/k8s_init.sh"     
+  }
+
+  provisioner "remote-exec" {
+    connection {
+    type = "ssh"
+    user = "homelab"
+    private_key = "${file("/home/homelab/.ssh/id_rsa")}"
+    host = "192.168.219.45"
+  }
+    inline = [
+      "sudo rm -rf /var/lib/apt/lists/*",
+      "sudo apt update -o Acquire::CompressionTypes::Order::=gz",
+      "sudo /bin/bash k8s_init.sh --version v1.28"
+    ]    
+  }
+}
+
 ## ksg
 resource "proxmox_virtual_environment_vm" "ksg-worker01" {
   name = "ksg-worker01"
