ac2bc6994eb0e967eaf75d6ebc917f8095fb0c87
Waji-97 <wajiwos16@gmail.com>
2024-04-28 13:43:10 +0900

    add yg nodes

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -8,7 +8,19 @@ locals {
   ksg_worker_nodes = [
     for i in range(1, var.ksg_worker_count + 1) : {
       name    = "ksg-worker0${i}"
-      address = cidrhost(var.cluster_node_network, var.ksg_worker_ip + i)
+      address = cidrhost(var.cluster_node_network, var.yg_worker_ip + i)
+    }
+  ]
+  yg_master_nodes = [
+    for i in range(1, var.yg_master_count + 1) : {
+      name    = "yg-master0${i}"
+      address = cidrhost(var.cluster_node_network, var.yg_master_ip + i)
+    }
+  ]
+  yg_worker_nodes = [
+    for i in range(1, var.yg_worker_count + 1) : {
+      name    = "yg-worker0${i}"
+      address = cidrhost(var.cluster_node_network, var.yg_worker_ip + i)
     }
   ]
 }
--- terraform/master.tf
+++ terraform/master.tf
@@ -1,5 +1,5 @@
 ## KSG Cluster Node Resources
-resource "proxmox_virtual_environment_vm" "master" {
+resource "proxmox_virtual_environment_vm" "ksg_master" {
   count     = var.ksg_master_count
   name      = local.ksg_master_nodes[count.index].name
   node_name = var.proxmox_server
@@ -17,6 +17,50 @@ resource "proxmox_virtual_environment_vm" "master" {
     ip_config {
       ipv4 {
         address = "${local.ksg_master_nodes[count.index].address}/24"
+        gateway = var.cluster_node_network_gateway
+      }
+    }
+
+    user_account {
+      username = "homelab"
+      keys     = [trimspace(var.ssh_key)]
+    }
+
+  }
+
+
+  disk {
+    datastore_id = "local-lvm"
+    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_image_pve.id
+    interface    = "virtio0"
+    iothread     = true
+    size         = 20
+  }
+
+  network_device {
+    bridge = "vmbr0"
+  }
+}
+
+## YG Master Nodes
+resource "proxmox_virtual_environment_vm" "yg_master" {
+  count     = var.yg_master_count
+  name      = local.yg_master_nodes[count.index].name
+  node_name = var.proxmox_server
+
+  cpu {
+    cores = 2
+  }
+
+  memory {
+    dedicated = 2048
+  }
+
+  initialization {
+    upgrade = false
+    ip_config {
+      ipv4 {
+        address = "${local.yg_master_nodes[count.index].address}/24"
         gateway = var.cluster_node_network_gateway
       }
     }
