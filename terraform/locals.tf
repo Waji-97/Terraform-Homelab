86c9ad07b21d8c60126e58db50b81b69f5948269
Waji-97 <wajiwos16@gmail.com>
2024-04-28 20:44:26 +0900

    delete ksg resources

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,16 +1,4 @@
 locals {
-  ksg_master_nodes = [
-    for i in range(1, var.ksg_master_count + 1) : {
-      name    = "ksg-master0${i}"
-      address = cidrhost(var.cluster_node_network, var.ksg_master_ip + i)
-    }
-  ]
-  ksg_worker_nodes = [
-    for i in range(1, var.ksg_worker_count + 1) : {
-      name    = "ksg-worker0${i}"
-      address = cidrhost(var.cluster_node_network, var.ksg_worker_ip + i)
-    }
-  ]
   yg_master_nodes = [
     for i in range(1, var.yg_master_count + 1) : {
       name    = "yg-master0${i}"
