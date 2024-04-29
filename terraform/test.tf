79070f61af542a47bbc1883163bf6f961fb6eabd
Waji <76054852+Waji-97@users.noreply.github.com>
2024-04-30 00:26:09 +0900

    Update test.tf

--- terraform/test.tf
+++ terraform/test.tf
@@ -1,46 +1,27 @@
 resource "proxmox_virtual_environment_vm" "example" {
   name      = "test-talos"
   node_name = "pve"
-  bios = "ovmf"
+  started = false
 
   clone {
     vm_id = 3000
   }
 
   machine = "q35"
-
-  operating_system {
-    type = "l26"
+ 
+  memory {
+    dedicated = 2048
   }
 
   cpu {
-    type = "host"
     cores = 2
-  }
-
-  memory {
-    dedicated = 2 * 1024
   }
 
   disk {
     datastore_id = "local-lvm"
-    interface    = "scsi0"
+    interface    = "virtio0"
     iothread     = true
-    ssd          = true
-    discard      = "on"
     size         = 20
-    file_format  = "raw"
-  }
-
-  agent {
-    enabled = true
-    trim    = true
-  }
-
-  efi_disk {
-    datastore_id = "local-lvm"
-    file_format  = "raw"
-    type         = "4m"
   }
 
   initialization {
